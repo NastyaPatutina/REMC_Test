d5bff233a6aeee4117490ac94e498fe64618c17f
Nastya <npatutina@gmail.com>
2019-02-17 20:04:11 +0300

    Добавлен контроллер для Зданий

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,2 +1,49 @@
-class BuildinsController
+class BuildingsController < ApplicationController
+  before_action :set_building, only: [:show, :update, :destroy]
+
+  # GET /buildings
+  def index
+    @buildings = Building.all
+    render json: @buildings
+  end
+
+  # GET /buildings/1
+  def show
+    render json: @building
+  end
+
+  # POST /buildings
+  def create
+    @building = Building.new(building_params)
+    if @building.save
+      render json: @building, status: :created, location: building_url(@building)
+    else
+      render json: @building.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /buildings/1
+  def update
+    if @building.update(building_params)
+      render json: @building
+    else
+      render json: @building.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /buildings/1
+  def destroy
+    @building.destroy
+  end
+
+  private
+
+  # Задает здание
+  def set_building
+    @building = Building.find(params[:id])
+  end
+
+  def building_params
+    params.require(:building).permit(Building.column_names)
+  end
 end
