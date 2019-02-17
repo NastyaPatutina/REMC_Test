197ac3d45841b8a03e2ff705ba15f6d5437e0a76
Nastya <npatutina@gmail.com>
2019-02-17 19:11:14 +0300

    Добавлена аннотация к моделям

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: buildings
+#
+#  id             :bigint(8)        not null, primary key
+#  building_class :string           not null
+#  street         :string           not null
+#  house_number   :string           not null
+#  floors         :integer          default(0), not null
+#
+
 class Building < ApplicationRecord
  has_many :blocks
 end
